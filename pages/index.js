f1b3f5be270acee78c321df4b176bf9f03b476ce
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-03-12 05:17:22 +0300

    add: base next and chakra

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Home() {
+  const { toggleColorMode } = useColorMode();
+  const backgroundColor = useColorModeValue("gray.100", "gray.700");
+  return (
+    <Flex
+      height="100vh"
+      alignItems="flex-start"
+      justifyContent="center"
+      pt={150}
+    >
+      <Flex direction="column" background={backgroundColor} p={12} rounded={6}>
+        <Heading mb={6}>Login</Heading>
+        <Input placeholder="test input" variant="filled" mb={3} type="text" />
+        <Button mb={6} colorScheme="teal">
+          submit
+        </Button>
+        <Button onClick={toggleColorMode}>change theme</Button>
+      </Flex>
+    </Flex>
+  );
+}
