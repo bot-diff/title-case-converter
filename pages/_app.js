ccf2fe6d581ad96ea6c2d1d3d0d97fe0aa5107eb
amjadh96 <amgadhabib96@gmail.com>
2022-03-12 22:02:30 +0100

    i18next

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import './i18n'
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+        <I18nextProvider i18n={i18next}>
+          <Component {...pageProps} />
+        </I18nextProvider>
     </ChakraProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
 import {
   Flex,
+  Select,
   Heading,
   Input,
   Button,
   useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { FaGlobeAfrica } from 'react-icons/fa'
+import { useTranslation } from 'react-i18next'
 
 export default function Home() {
+  const { t, i18n } = useTranslation()
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng)
+  };
+
   const { toggleColorMode } = useColorMode()
   const backgroundColor = useColorModeValue('gray.100', 'gray.700')
-  return (
+  return (<>
     <Flex
+      direction="column"
       height="100vh"
-      alignItems="flex-start"
+      alignItems="center"
       justifyContent="center"
       pt={150}
     >
       <Flex direction="column" background={backgroundColor} p={12} rounded={6}>
-        <Heading mb={6}>Login</Heading>
+        <Heading mb={6}>{t('login')}</Heading>
         <Input placeholder="test input" variant="filled" mb={3} type="text" />
         <Button mb={6} colorScheme="teal">
           submit
@@ -26,5 +35,14 @@ export default function Home() {
         <Button onClick={toggleColorMode}>change theme</Button>
       </Flex>
     </Flex>
+    <Flex>
+      <Flex dir='row' alignItems='center'>
+        <Select icon={<FaGlobeAfrica />} width={150} size="sm" mb={3} id='lang' onChange={(e) => changeLanguage(e.target.value)}>
+          <option value="en" selected>English</option>
+          <option value="de">Deutsch</option>
+        </Select>
+      </Flex>
+    </Flex>
+  </>
   )
 }
