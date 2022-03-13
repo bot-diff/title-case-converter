75ac388961c14076df67c1f525ac60fd9916d2c6
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-03-13 03:41:43 +0300

    add: improve layout

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,22 @@ import {
   Button,
   useColorMode,
   useColorModeValue,
+  Stack,
+  Text,
+  Select,
+  IconButton,
+  Spacer,
+  Tooltip,
 } from '@chakra-ui/react'
+import { FaGlobeAfrica } from 'react-icons/fa'
+import { MdContentCopy } from 'react-icons/md'
 
 export default function Home() {
   const { toggleColorMode } = useColorMode()
-  const backgroundColor = useColorModeValue('gray.100', 'gray.700')
+  const backgroundColor = useColorModeValue(
+    'rgb(79, 128, 176, 0.075)',
+    'rgb(237, 242, 247, 0.1)'
+  )
   return (
     <Flex
       height="100vh"
@@ -17,12 +28,64 @@ export default function Home() {
       justifyContent="center"
       pt={150}
     >
-      <Flex direction="column" background={backgroundColor} p={12} rounded={6}>
-        <Heading mb={6}>Login</Heading>
-        <Input placeholder="test input" variant="filled" mb={3} type="text" />
-        <Button mb={6} colorScheme="teal">
-          submit
-        </Button>
+      {/* TODO: wrap a github icon button with Flex row (as a header navbar) */}
+      <Flex
+        direction="column"
+        width="100vh"
+        // background={backgroundColor}
+        p={12}
+        // rounded={6}
+      >
+        <Stack spacing={2} mb={5}>
+          <Heading as="h2" align="center" size="3xl">
+            Title Case Converter
+          </Heading>
+          <Text align="center" fontSize="2xl">
+            Convert any text into a title case format.
+          </Text>
+        </Stack>
+        <Input
+          placeholder="histoire de la baguette, de la baguette à la tradition"
+          variant="outline"
+          _placeholder={{ color: 'gray.400' }}
+          fontSize="20px"
+          mb={3}
+          size="lg"
+          type="text"
+          textAlign="center"
+          bg="gray.100"
+          color="gray.700"
+        />
+        <Select icon={<FaGlobeAfrica />} width={150} size="sm" mb={1}>
+          <option value="en">English</option>
+          <option value="fr">Français</option>
+        </Select>
+
+        <Flex dir="row" mb={3} alignItems="end">
+          <Text align="left" fontSize="xl">
+            The result:
+          </Text>
+          <Spacer />
+          <Tooltip label="Copy" placement="auto" hasArrow>
+            <IconButton
+              colorScheme="blue"
+              aria-label="Copy to clipboard"
+              icon={<MdContentCopy />}
+            />
+          </Tooltip>
+        </Flex>
+        <Flex
+          maxWidth="100vh"
+          height="25vh"
+          style={{ backgroundColor }}
+          rounded={10}
+        >
+          <Text p={4} fontSize="xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
+            nulla pellentesque dignissim enim.
+          </Text>
+        </Flex>
         <Button onClick={toggleColorMode}>change theme</Button>
       </Flex>
     </Flex>
