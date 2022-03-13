7e5d2b614a54b05d18e966d56562dce3165c278b
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-03-13 07:37:41 +0300

    add: header nav bar, french language, mode switcher improvements

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -3,37 +3,23 @@ import { initReactI18next } from 'react-i18next'
 
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
 
-import common_de from '../public/locales/de/translation.json'
-import common_en from '../public/locales/en/translation.json'
+import en_translation from './locales/en/translation.json'
+import de_translation from './locales/de/translation.json'
+import fr_translation from './locales/fr/translation.json'
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     lng: 'en',
     fallbackLng: 'en',
     debug: true,
-
     resources: {
-      en: { translation: common_en },
-      de: { translation: common_de },
-    },
-
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      en: { translation: en_translation },
+      de: { translation: de_translation },
+      fr: { translation: fr_translation },
     },
   })
 
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import {
   Select,
   Heading,
   Input,
-  Button,
   useColorMode,
   useColorModeValue,
   Stack,
@@ -11,49 +10,102 @@ import {
   IconButton,
   Spacer,
   Tooltip,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from '@chakra-ui/react'
-import { FaGlobeAfrica } from 'react-icons/fa'
-import { MdContentCopy } from 'react-icons/md'
+import { FaGlobeAfrica, FaGithub } from 'react-icons/fa'
+import { MdContentCopy, MdLightMode, MdDarkMode } from 'react-icons/md'
 import { useTranslation } from 'react-i18next'
 
 export default function Home() {
   const { t, i18n } = useTranslation()
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng)
+  const changeLanguageHandler = (lang) => {
+    i18n.changeLanguage(lang)
   }
 
   const { toggleColorMode } = useColorMode()
-  const backgroundColor = useColorModeValue(
-    'rgb(79, 128, 176, 0.075)',
-    'rgb(237, 242, 247, 0.1)'
-  )
+
   return (
     <>
-      <Flex
-        height="100vh"
-        alignItems="center"
-        justifyContent="center"
-        direction="column"
-        pt={150}
-      >
-        {/* TODO: wrap a github icon button with Flex row (as a header navbar) */}
-        <Flex
-          direction="column"
-          width="100vh"
-          // background={backgroundColor}
-          p={12}
-          // rounded={6}
+      <Flex dir="row" alignItems="center" py={4} px={5}>
+        <Tooltip label={t('Source code on GitHub')} placement="right" hasArrow>
+          <IconButton
+            variant="outline"
+            aria-label="Source code"
+            onClick={() =>
+              window.open('https://github.com/bot-diff/title-case-converter')
+            }
+            icon={<FaGithub />}
+          />
+        </Tooltip>
+        <Spacer />
+        <Tooltip
+          label={useColorModeValue(
+            t('Switch to dark mode'),
+            t('Switch to light mode')
+          )}
+          placement="left"
+          hasArrow
         >
+          <IconButton
+            variant="outline"
+            aria-label="Switch mode"
+            onClick={toggleColorMode}
+            icon={useColorModeValue(<MdDarkMode />, <MdLightMode />)}
+            mr={2}
+          />
+        </Tooltip>
+        <Menu>
+          <Tooltip label={t('Change language')} placement="bottom-end" hasArrow>
+            <MenuButton
+              as={IconButton}
+              aria-label="Languages"
+              icon={<FaGlobeAfrica />}
+              variant="outline"
+            />
+          </Tooltip>
+          <MenuList>
+            <MenuItem
+              icon={<>🇬🇧</>}
+              onClick={() => changeLanguageHandler('en')}
+            >
+              English
+            </MenuItem>
+            <MenuItem
+              icon={<>🇩🇪</>}
+              onClick={() => changeLanguageHandler('de')}
+            >
+              Deutsch
+            </MenuItem>
+            <MenuItem
+              icon={<>🇫🇷</>}
+              onClick={() => changeLanguageHandler('fr')}
+            >
+              Français
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+
+      <Flex
+        height="90vh"
+        alignItems="center"
+        justifyContent="start"
+        direction="column"
+      >
+        <Flex direction="column" width="100vh" p={12} pt={125}>
           <Stack spacing={2} mb={5}>
             <Heading as="h2" align="center" size="3xl">
               {t('Title Case Converter')}
             </Heading>
             <Text align="center" fontSize="2xl">
-              Convert any text into a title case format.
+              {t('Convert any text into a title case format.')}
             </Text>
           </Stack>
           <Input
-            placeholder="histoire de la baguette, de la baguette à la tradition"
+            placeholder={t('the quick brown fox jumps over the lazy dog')}
             variant="outline"
             _placeholder={{ color: 'gray.400' }}
             fontSize="20px"
@@ -64,17 +116,20 @@ export default function Home() {
             bg="gray.100"
             color="gray.700"
           />
+
           <Select icon={<FaGlobeAfrica />} width={150} size="sm" mb={1}>
-            <option value="en">English</option>
+            <option value="en" selected>
+              English
+            </option>
             <option value="fr">Français</option>
           </Select>
 
           <Flex dir="row" mb={3} alignItems="end">
             <Text align="left" fontSize="xl">
-              The result:
+              {t('The result:')}
             </Text>
             <Spacer />
-            <Tooltip label="Copy" placement="auto" hasArrow>
+            <Tooltip label={t('Copy')} placement="auto" hasArrow>
               <IconButton
                 colorScheme="blue"
                 aria-label="Copy to clipboard"
@@ -86,33 +141,18 @@ export default function Home() {
           <Flex
             maxWidth="100vh"
             height="25vh"
-            style={{ backgroundColor }}
+            style={{
+              backgroundColor: useColorModeValue(
+                'rgb(79, 128, 176, 0.075)',
+                'rgb(237, 242, 247, 0.1)'
+              ),
+            }}
             rounded={10}
           >
             <Text p={4} fontSize="xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
-              nulla pellentesque dignissim enim.
+              {t('Lorem ipsum')}
             </Text>
           </Flex>
-          <Button onClick={toggleColorMode}>change theme</Button>
-        </Flex>
-      </Flex>
-      <Flex>
-        <Flex dir="row" alignItems="center">
-          <Select
-            icon={<FaGlobeAfrica />}
-            width={150}
-            size="sm"
-            mb={3}
-            id="lang"
-            onChange={(e) => changeLanguage(e.target.value)}
-          >
-            <option value="en" selected>
-              English
-            </option>
-            <option value="de">Deutsch</option>
-          </Select>
         </Flex>
       </Flex>
     </>
