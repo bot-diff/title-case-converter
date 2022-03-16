2a752fa90ce29577f3c12889a5346980084fef2f
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-03-16 07:41:52 +0300

    add: title, description meta tag, icon

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { useTranslation } from 'react-i18next'
 import HeaderNavbar from '../components/HeaderNavbar'
 import MainBody from '../components/MainBody'
@@ -6,6 +7,14 @@ export default function Home() {
   const { t, i18n } = useTranslation()
   return (
     <>
+      <Head>
+        <title>Title Case Converter</title>
+        <meta
+          name="description"
+          content="Convert any text into a title case format"
+        />
+        <link rel="icon" href="/icon.ico" />
+      </Head>
       <HeaderNavbar t={t} i18n={i18n} />
       <MainBody t={t} />
     </>
