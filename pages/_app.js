a36343b2f6d1d3503943418fc811848452b92aa9
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-03-18 05:35:19 +0300

    refactor: use dark theme by default

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import '../i18n/i18n'
 import { I18nextProvider } from 'react-i18next'
+import theme from '../utils/theme'
 import i18next from 'i18next'
+import '../i18n/i18n'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <I18nextProvider i18n={i18next}>
         <Component {...pageProps} />
       </I18nextProvider>
